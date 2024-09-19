0d3b3445cdf38a62c619b1cdd36d335782087641
jainil2472 <pjainil002@gmail.com>
2024-09-19 21:48:02 +0530

    third commit

--- app/src/main/java/com/example/foodcall/HomeFragment.java
+++ app/src/main/java/com/example/foodcall/HomeFragment.java
@@ -1,19 +1,37 @@
 package com.example.foodcall;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class HomeFragment extends Fragment {
-
+    ImageView burger,pizza;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        // Inflate the layout for this fragment
+        View rootView = inflater.inflate(R.layout.fragment_home, container, false);
+
+        TextView burgertext = rootView.findViewById(R.id.burgertext);
+        burger = rootView.findViewById(R.id.burger);
+
+        burger.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), Burger.class);
+                startActivity(intent);
+
+            }
+        });
+        return rootView;
     }
 }
 
